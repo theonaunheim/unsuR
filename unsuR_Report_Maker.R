f752b488bf48f22e5b72b4d5c71e6f30679964ed
unknown <cneskey@protonmail.com>
2020-06-06 11:16:32 -0400

    Added output folder, updated links.

--- unsuR_Report_Maker.R
+++ unsuR_Report_Maker.R
@@ -5,7 +5,8 @@ library(flexdashboard)
 # Render the markdown file with flex dashboard.
 start_time <- Sys.time()
 
-render("src/unsuR_Report.Rmd",
+render(output_dir = "output/",
+       "src/unsuR_Report.Rmd",
        flex_dashboard(logo = "../imgs/emblem.png",
                       favicon = "../imgs/favicon.png",
                       theme="united",
@@ -13,10 +14,10 @@ render("src/unsuR_Report.Rmd",
                       source_code = "https://github.com/cneskey/unsuR"))
   
 # Launch the report.
-browseURL("src/unsuR_Report.html")
+browseURL("output/unsuR_Report.html")
 
 # Clean Up
-unlink("*_temp.xlsx", recursive = TRUE)
+unlink("*_temp.xlsx")
 
 # Return time stats
 end_time <- Sys.time()
